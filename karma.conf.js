a32153fdd019277546cf9306596927b559c74726
SJO3662 <soline.john@etudiant.univ-reims.fr>
2017-06-26 16:44:42 +0200

    [test] Configuration des test

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/main/webapp/src/test/test.js',
+      'src/main/webapp/test/unit/specs/test.spec.js',
     ],
 
 
--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import store from './vuex/store'
@@ -13,7 +14,7 @@ Vue.use(Vuex);
 Vue.use(Dropdown);
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+// eslint-disable no-new
 new Vue({
   el: '#app',
 
@@ -22,3 +23,16 @@ new Vue({
   config,
   template: `<div><router-view></router-view></div>`
 });
+
+
+/*import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/!* eslint-disable no-new *!/
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})*/
