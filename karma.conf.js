80da9b025cd8d104d5f7aeb596e451ac829a4b8e
SJO3662 <soline.john@etudiant.univ-reims.fr>
2017-06-27 17:34:30 +0200

    [test] Test registrationPage.vue

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/main/webapp/test/unit/specs/test.spec.js',
+      'src/main/webapp/test/unit/specs/registrationPage.spec.js',
     ],
 
 
