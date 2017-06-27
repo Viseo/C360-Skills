23058d2d8ce0d9d1e541b45158bd91e6000cb31d
SJO3662 <soline.john@etudiant.univ-reims.fr>
2017-06-27 11:56:41 +0200

    [test] Configuration des test

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import Vue from 'vue'
+/*import Vue from 'vue'
 import Vuex from 'vuex'
 import VueStrap from 'vue-strap'
 import Router from 'vue-router'
@@ -22,14 +22,14 @@ new Vue({
 });
 
 
-/*import Vue from 'vue'
+import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
 
-/!* eslint-disable no-new *!/
+// eslint-disable no-new
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})*/
+})
