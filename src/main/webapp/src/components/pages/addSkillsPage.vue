98cbd8027e49e34bb910faf7854d1d26b0856c7a
Caroline <caroline.l-hote@edu.ece.fr>
2017-06-29 17:28:35 +0200

    [US#GCo8] Ajustement input

--- src/main/webapp/src/components/pages/addSkillsPage.vue
+++ src/main/webapp/src/components/pages/addSkillsPage.vue
@@ -17,8 +17,8 @@
         </g>
         <g v-for="(skill,i) in skills">
           <customCircle :id="skill.id" :cx="positionX(i)" :cy="positionY(i)" :content="skill.label" stroke="#E03559" fill="white"
-                        @click="selectSkill(skill)" :showCircleBlur="showCircleBlurOrNot(skill.id)"/>
-          <foreignObject v-show="selectedSkill.skill1.id == skill.id" :x="positionX(i) - 44" :y="positionY(i)-10">
+                        @click="selectSkill(skill)"/>
+          <foreignObject v-show="selectedSkill.skill1.id == skill.id" :x="positionX(i) - 44" :y="positionY(i)-16">
             <div xmlns="http://www.w3.org/1999/xhtml">
               <form @submit.prevent="updateSkill">
                 <input class="inputCircle" maxlength="10" type="text" v-model="selectedSkill.skill1.label"/>
