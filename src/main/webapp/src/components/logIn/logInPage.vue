7404709d8ed25189c3fadf956437d8ba1197d02e
HBI1 <hamza.bourakkadi@gmail.com>
2017-07-04 11:29:34 +0200

    [develop] Modification des redirections à l'inscription

--- src/main/webapp/src/components/logIn/logInPage.vue
+++ src/main/webapp/src/components/logIn/logInPage.vue
@@ -169,6 +169,7 @@
       isAdministratorAuthenticated() {
         var userToken = localStorage.getItem('token');
         var isAdministrator = jwtDecode(userToken).isAdmin;
+        console.log("heheh"+isAdministrator);
         return isAdministrator;
       },
 
