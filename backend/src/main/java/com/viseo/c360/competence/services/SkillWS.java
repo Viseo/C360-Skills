09f3cac41780b9a772d634b1e27b1f28aafbab94
Unknown <guyuanhao@foxmail.com>
2017-10-16 14:41:16 +0200

    [dev] Synchroniser les compétence 1

--- backend/src/main/java/com/viseo/c360/competence/services/SkillWS.java
+++ backend/src/main/java/com/viseo/c360/competence/services/SkillWS.java
@@ -1,5 +1,10 @@
 package com.viseo.c360.competence.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.GetResponse;
+import com.viseo.c360.competence.amqp.InformationMessage;
 import com.viseo.c360.competence.converters.skill.DescriptionToSkill;
 import com.viseo.c360.competence.converters.skill.SkillToDescription;
 import com.viseo.c360.competence.dao.ExpertiseDAO;
@@ -9,12 +14,17 @@ import com.viseo.c360.competence.exceptions.C360Exception;
 import com.viseo.c360.competence.exceptions.dao.UniqueFieldException;
 import com.viseo.c360.competence.exceptions.dao.util.ExceptionUtil;
 import com.viseo.c360.competence.exceptions.dao.util.UniqueFieldErrors;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.ChannelCallback;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.core.convert.ConversionException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.persistence.PersistenceException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by HBO3676 on 15/06/2017.
@@ -29,6 +39,111 @@ public class SkillWS {
 
     @Inject
     ExceptionUtil exceptionUtil;
+
+    @Inject
+    Queue responseCompetence;
+
+    @Inject
+    FanoutExchange fanout;
+
+    @Inject
+    RabbitTemplate rabbitTemplate;
+
+    public SkillWS(){
+        TimerTask timerTask = new synchronizeDateBase();
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(timerTask, 0, 10*1000);
+        System.out.println("Synchronize DateBase for skill data started");
+    }
+
+    private class synchronizeDateBase extends TimerTask{
+
+        @Override
+        public void run() {
+            List<SkillDescription> otherListSkill = getSkillsFromOtherServices();
+            handleReceivedCollaborator(otherListSkill);
+            System.out.println("Skill data is synchronized");
+        }
+    }
+
+    private List<SkillDescription> mergeTwoSkillList (List<SkillDescription> listSkill, List<SkillDescription> otherListSkill){
+        if(listSkill == null){
+            return otherListSkill;
+        }
+        else if (otherListSkill == null){
+            return listSkill;
+        }
+        Set<String> labels = otherListSkill.stream().map(SkillDescription::getLabel).collect(Collectors.toSet());
+        List<SkillDescription> mergedListSkill = listSkill.stream().filter(skill -> !labels.contains(skill.getLabel()))
+                .collect(Collectors.toList());
+        return mergedListSkill;
+    }
+
+    private List<SkillDescription> handleReceivedCollaborator (List<SkillDescription> listReceived){
+        if(listReceived != null){
+            for (SkillDescription s : listReceived){
+                if (!skillDAO.getSkillByLabel(s.getLabel())){
+                    skillDAO.addSkill(new DescriptionToSkill().convert(s));
+                }
+            }
+        }
+        return new SkillToDescription().convert(skillDAO.getAllSkills());
+    }
+
+    private List<SkillDescription> getSkillsFromOtherServices(){
+        ObjectMapper mapperObj = new ObjectMapper();
+        UUID personalMessageSequence = UUID.randomUUID();
+        InformationMessage informationMessage = new InformationMessage()
+                .setNameFileResponse(responseCompetence.getName())
+                .setMessageDate(new Date())
+                .setSequence(personalMessageSequence);
+        try{
+            this.rabbitTemplate.convertAndSend(fanout.getName(),"",mapperObj.writeValueAsString(informationMessage));
+            InformationMessage result;
+            result = this.rabbitTemplate.execute(new ChannelCallback<InformationMessage>(){
+
+                @Override
+                public InformationMessage doInRabbit(Channel channel) throws Exception {
+                    long startTime = System.currentTimeMillis();
+                    long elapsedTime;
+                    InformationMessage collectedResponse = new InformationMessage();
+                    GetResponse consumerResponse;
+                    long deliveryTag;
+                    sleep();
+                    do{
+                        elapsedTime = (new Date()).getTime() - startTime;
+                        consumerResponse = channel.basicGet(responseCompetence.getName(), false);
+                        if (consumerResponse != null){
+                            deliveryTag = consumerResponse.getEnvelope().getDeliveryTag();
+                            InformationMessage rabbitMessageResponse = new ObjectMapper().readValue(consumerResponse.getBody(), InformationMessage.class);
+                            channel.basicAck(deliveryTag, true);
+                            if ((new Date().getTime() - rabbitMessageResponse.getMessageDate().getTime()) < 50000) {
+                                if (rabbitMessageResponse.getSequence().equals(personalMessageSequence)) {
+                                    collectedResponse.setSkillsDescription(mergeTwoSkillList(collectedResponse.getSkillsDescription()
+                                            , rabbitMessageResponse.getSkillsDescription()));
+                                } else {
+                                    channel.basicPublish("", responseCompetence.getName(), null, consumerResponse.getBody());
+                                }
+                            }
+                        }
+                    }while(elapsedTime < 2000);
+                    // the expiration time is 2s, during this 2s, service will continue to receive the skill list
+                    return collectedResponse;
+                }
+            });
+            return result.getSkillsDescription();
+
+        }catch (JsonProcessingException jpe){
+            throw new RuntimeException(jpe);
+        }
+    }
+
+    public void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+    }
 
     @CrossOrigin
     @RequestMapping(value = "${endpoint.addskill}", method = RequestMethod.POST)
